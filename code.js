ac31491e1dd7ba38735abf0fb19b6537d6d75cda
Tanmay Patil <37.tanmay@gmail.com>
2023-01-12 19:31:09 +0530

    Alternate syntax for error handling

--- code.js
+++ code.js
@@ -1,47 +1,41 @@
 const code = `
-Integer
-func square(Integer num) {
+func square(Integer num) -> Integer {
     num * num
 }
 
-Integer
-func sumOfSquares(Integer a, Integer b) {
+func sumOfSquares(Integer a, Integer b) -> Integer {
     square(a) + square(b)
 }
 
 
-ok(Address address) or outOfMemory()
-proc alloc(Integer size)
+proc alloc(Integer size) -> choice {
+                                case ok(Address address) 
+                                case outOfMemory
+                            }
 
-ok(Address address) or outOfMemory()
-proc realloc(Address previousLocation, Integer newSize)
+proc realloc(Address previousLocation, Integer newSize) -> | ok(Address address)
+                                                           | outOfMemory
 
-ok() or err(Trace trace)
-proc dealloc(Address location)
-
-
-created(FileHandle handle) or failed()
-proc createFile(String path)
-
-opened(FileHandle handle) or fileNotFound()
-proc openFile(String path)
-
-Integer
-proc fileSize(FileHandle handle)
-
-ok() or endOfFile(Integer bytesRead) or err(Trace trace)
-proc readFile(FileHandle handle, Address address, Integer length)
+proc dealloc(Address location) -> ok or err(Trace trace)
 
 
-ok(named String userName) or Error
-proc readUserNameFromFile(String filePath) {
+proc createFile(String path) -> created(FileHandle handle) or failed
+
+proc openFile(String path) -> opened(FileHandle handle) or fileNotFound
+
+proc fileSize(FileHandle handle) -> Integer
+
+proc readFile(FileHandle handle, Address address, Integer length) -> ok() or endOfFile(Integer bytesRead) or err(Trace trace)
+
+
+proc readUserNameFromFile(String filePath) -> ok(named String userName) or err(Trace trace) {
     match openFile(filePath)
-    case notFound => Error.FileNotFound(filePath)
-    case FileHandle handle => {
+    case fileNotFound => Error.FileNotFound(filePath)
+    case opened(FileHandle handle) => {
         let fileSize = fileSize(handle);
         let userName = makeString(fileSize);
         match readFile(handle, userName.address, fileSize)
-        case fail(Error e) => e
+        case err(Trace trace) => err(trace)
         case endOfFile(Integer bytesRead) => resizeString(userName, bytesRead)
         case ok => userName
     }
