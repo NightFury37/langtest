f19fefb51480ce25d01b9f961927cd7a63ac8cd1
Tanmay Patil <37.tanmay@gmail.com>
2023-01-17 17:55:56 +0530

    Added return keyword

--- code.js
+++ code.js
@@ -5,18 +5,38 @@ func sumOfSquares(Integer first, Integer second) = square(first) + square(second
 
 func fibonacci(Integer count, Integer a = 0, Integer b = 1) {
     match count < 1
-    case true: => a
+    case true: => return a
     case false: => goto fibonacci(count = count - 1, a = b, b = a + b)
 }
 
 func factorial(Integer number, Integer result = 1) {
     match number < 2
-    case true: => result
+    case true: => return result
     case false: => goto factorial(number = number - 1, result = result * number)
 }
 
 func compose<Type A, Type B, Type C>(func f(A a) -> B, func g(B b) -> C) {
-    (A x) => g(f(x))
+    return (A x) => g(f(x))
+}
+
+proc composeTest(String input) {
+    let hFun = compose(foo, bar);
+    let message = {
+        match hFun(input)
+        case true: => "Less than 5 characters"
+        case false: => "More than 4 characters"
+    }
+    return toUpperCase(message)
+
+    where
+
+    func foo(String s) {
+        return length(s)
+    }
+
+    func bar(Integer i) {
+        return i < 5
+    }
 }
 
 
